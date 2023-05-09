d17794220c2bc3cea244712c7cc14da432e881f4
Evgeniy <gormoshka123@gmail.com>
2023-05-09 19:53:44 +0300

    denamically redered  expenses array in the ExpensesListComponent

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -7,7 +7,7 @@ import ExpensesFilter from "../NewExpense/ExpensesFilter";
 const ExpensesList = (props) => {
   const [year, setYear] = useState("2020");
 
-  const addYearExpnseHandler = (choosenYear) => {
+  const addYearExpenseHandler = (choosenYear) => {
     setYear(choosenYear);
   };
 
@@ -15,29 +15,16 @@ const ExpensesList = (props) => {
     <div>
       <Card className="expenses">
         <ExpensesFilter
-          onAddYearExpense={addYearExpnseHandler}
+          onAddYearExpense={addYearExpenseHandler}
           selectedYear={year}
         />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
