dddba3b5cbe4e40ec640f5ed84b211da447f4314
Evgeniy <gormoshka123@gmail.com>
2023-05-12 20:35:03 +0300

    Added JSX and caltculation maxValue  for chartBar component

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,7 +1,17 @@
 import "./ChartBar.css";
 
-const ChartBar = () => {
-  return <div></div>;
+const ChartBar = (props) => {
+  let barFillhaigt = "0%";
+  if (props.maxValue > 0)
+    barFillhaigt = Math.round((props.value / props.maxValue) * 100) + "%";
+  return (
+    <div className="chart-bar">
+      <div className="chart-bar__inner">
+        <div className="chart-bar__fill" style={{ hight: barFillhaigt }}></div>
+      </div>
+      <div className="chart-bar__label">{props.label}</div>
+    </div>
+  );
 };
 
 export default ChartBar;
